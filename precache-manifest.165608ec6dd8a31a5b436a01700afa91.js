2ee014125c77fdde8580db5bee14ffbed4b1ef0d
padumamadushanka <madushankapaduma@gmail.com>
2020-01-26 11:50:30 +0530

    Updates

---
+++ precache-manifest.165608ec6dd8a31a5b436a01700afa91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86efca042d1038ea6153a291723efc87",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "01128ae02c626f906c30",
+    "url": "/my-portfolio/static/css/main.d9e28f1c.chunk.css"
+  },
+  {
+    "revision": "732bfc079f5a633ba3d0",
+    "url": "/my-portfolio/static/js/2.fe1fd49a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-portfolio/static/js/2.fe1fd49a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01128ae02c626f906c30",
+    "url": "/my-portfolio/static/js/main.96bed206.chunk.js"
+  },
+  {
+    "revision": "164cd71dc8e56a933ce3",
+    "url": "/my-portfolio/static/js/runtime-main.d96a2d46.js"
+  },
+  {
+    "revision": "32ec8767ffe385ebe99570731a3455df",
+    "url": "/my-portfolio/static/media/devblog.32ec8767.png"
+  },
+  {
+    "revision": "5a19eff4ddfab2638b1fa51ebcbedbd0",
+    "url": "/my-portfolio/static/media/ecom.5a19eff4.png"
+  },
+  {
+    "revision": "4cff20d5021a560670b8245484211ab0",
+    "url": "/my-portfolio/static/media/padumaM .4cff20d5.png"
+  }
+]);
